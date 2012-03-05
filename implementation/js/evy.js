527613eea9df140ab93b81d831631b46db92e86e
Leo Accend <github.aeonic@spamgourmet.com>
2012-03-04 22:10:49 -0800

    Rewriting the Evy interpreter. At this point, an Evy program can be written as follows:
        var Hello = Evy('print "Hello, world!"');
        Hello.subscribe( "print", function() { console.log( arguments[0][0] ); } );
        Hello.execute();
    
    This is a complete implementation of the *Hello World* example.
    
    The implementation is broken apart into the following logical steps:
    
     1. Construct the Evy container.
     2. Create native event subscriptions.
     3. Build the Evy tree.
     4. Execute the root node of the tree.
    
    Currently, there is no logic for when descendants are executed. The next revision will
    support this.

--- implementation/js/evy.js
+++ implementation/js/evy.js
@@ -1,108 +1,214 @@
-function each( array, behavior ) {
-  if( !array ) { array = []; }
-  if( !behavior ) { behavior = function() {}; }
-  for( var i = 0; i < array.length; i++ ) {
-    behavior.call( array[i], array[i], i );
-  }
-}
-
 var Evy = ( function() {
 
-  var subscribers = {};
-  
-  function parseArg( token ) {
-    var arg = {
-      key: token.replace( /(.+)=(.+)/g, "$1" ),
-      value: token.replace( /(.+)=(.+)/g, "$2" )
+  var F = function( fn ) {
+    return {
+      cache: function() {
+        return function() {
+          return fn.apply( this, arguments );
+        };
+      }
     };
-    if( arg.key === token ) {
-      arg.key = undefined;
-    }
-    return arg;
   };
 
-  return {
+  var A = F( function( array ) {
 
-    publish: function( event, args ) {
-
-      if( !subscribers[event] ) {
-        return;
+    if( !( array instanceof Array ) ) { ( function( proxy ) {
+      if( !array ) { array = []; }
+      for( var i = 0; i < array.length; i++ ) {
+        proxy.push( array[i] );
       }
+      array = proxy;
+    } )( [] ); }
+    
+    return {
 
-      each( subscribers[event], function() {
-        this.behavior.apply( Evy, args );
-      } );
+      each: function( fn ) {
+        if( !array ) { return this; }
+        for( var i = 0; i < array.length; i++ ) {
+          fn.call( array[i] );
+        }
+        return this;
+      },
 
-    },
+      first: function() { return this.at(0); },
+      last: function() { return this.at( array.length - 1 ); },
+      at: function( index ) { return array[index]; },
+      push: function( item ) { array.push( item ); return this; },
+      pop: function() { return array.pop(); },
+      clone: function() { return A( array.slice(0) ); },
+      toString: function() { return array.toString(); }
 
-    subscribe: function( event, args, behavior ) {
+    };
 
-      if( !subscribers[event] ) {
-        subscribers[event] = [];
-      }
+  } ).cache();
 
-      subscribers[event].push( {
-        event: event,
-        args: args,
-        behavior: behavior
-      } );
+  var S = F( function( string ) {
 
-    },
+    return {
 
-    parse: function(script) {
+      toNode: F( function() {
 
-      var statements = [];
+        var node = {};
 
-      each( script.split("\n"), function( line ) {
+        node.name = string.replace(/^\s+/g,"");
+        node.depth = string.length - node.name.length;
+        node.children = [];
 
-        var statement = {};
+        return node;
 
-        statement.indent = line.replace( /^(\s*).+/g, "$1" ).length;
-        statement.publishing = true;
-        statement.args = [];
+      } ).cache()
 
-        each( line.replace( /^\s+(.+)/, "$1" ).split(/\s+/g), function(token, index) {
-        
-          switch(index) {
-            case 0:
-              if( token === "@" ) {
-                statement.publishing = false;
-              } else {
-                statement.event = token;
-              }
-              break;
-            case 1:
-              if( !statement.publishing ) {
-                statement.event = token;
-              } else {
-                statement.args.push(parseArg(token));
-              }
-              break;
-            default:
-              statement.args.push(parseArg(token));
-              break;
+    };
+
+  } ).cache();
+
+  var Evy = F( function( program ) {
+
+    var PUBLICATION = /^([^ ]+)(?: (.+))?$/gi;
+
+    return {
+
+      tree: function() {
+
+        var root = {
+          depth: -1,
+          name: undefined,
+          children: []
+        };
+
+        var ancestors = A([root]);
+
+        A( program.split("\n") ).each( function() {
+
+          var node = S(this).toNode();
+          var parent = ancestors.last();
+
+          if( !node.name ) {
+          } else if( node.depth > parent.depth ) {
+            parent.children.push( node );
+            ancestors.push( node );
+          } else {
+            ancestors.pop();
+            arguments.callee.apply( this, arguments );
           }
 
         } );
 
-        if( statement.event ) {
-          statements.push( statement );
+        return root;
+
+      },
+
+      execute: function( context ) {
+
+        if( !context ) { context = this.tree(); }
+
+        if( context.name ) {
+
+          var event = context.name.replace( PUBLICATION, "$1" );
+          var parameters = this.parse( context.name.replace( PUBLICATION, "$2" ) );
+
+          this.publish( event, parameters, context );
+
+        } else {
+
+          for( var i = 0; i < context.children.length; i++ ) {
+            arguments.callee.call( this, context.children[i] );
+          }
+
         }
 
-      } );
+      },
 
-      return statements;
+      parse: function( line, start, parameters ) {
 
-    },
-  
-    execute: function( statement ) {
-      if( statement.publishing ) {
-        Evy.publish( statement.event, statement.args );
-      } else {
-        Evy.subscribe( statement.event, statement.args, statement.behavior );
+        if( !line ) { line = ""; }
+        if( !start ) { start = 0; }
+        if( !parameters ) { parameters = []; }
+
+        if( start >= line.length ) { return parameters; }
+
+        var space  = line.indexOf( ' ', start );
+        var equals = line.indexOf( '=', start );
+        var quote  = line.indexOf( '"', start );
+
+        if( space < 0 ) { space = line.length; }
+        if( quote < 0 ) { quote = line.length + 1; }
+        if( equals < 0 ) { equals = line.length + 2; }
+
+        var token  = line.substring( start, space );
+
+        if( quote < space ) {
+          var matchingQuote = line.indexOf( '"', quote + 1 );
+          token = line.substring( start, matchingQuote + 1 );
+        }
+
+        var end = start + token.length;
+
+        var parameter = [];
+
+        if( equals < space ) {
+          parameter.push( line.substring( start, equals ) );
+          parameter.push( line.substring( equals + 1, end ) );
+        } else {
+          parameter.push( token );
+        }
+
+        for( var i = 0; i < parameter.length; i++ ) {
+          if( parameter[i].indexOf('"') > -1 ) {
+            parameter[i] = parameter[i].substring( 1, parameter[i].length - 1 );
+          }
+        }
+
+        parameters.push( parameter );
+
+        return arguments.callee( line, end + 1, parameters );
+
+      },
+
+      publish: function( event, parameters, context ) {
+
+        if( !context ) { context = {}; }
+
+        A( this.events[event] || [] ).each( function() {
+          this.behavior.apply( context, parameters );
+        } );
+
+      },
+
+      events: {},
+
+      subscribe: function( event, behavior, context ) {
+
+        if( !context ) { context = {}; }
+
+        var events = this.events[event] || [];
+
+        events.push( { context: context, behavior: behavior } );
+
+        this.events[event] = events;
+
+      },
+
+      unsubscribe: function( event, behavior, context ) {
+
+        if( !context ) { context = {}; }
+
+        var events = [];
+
+        A( this.events[event] || [] ).each( function() {
+          if( this.behavior !== behavior || this.context !== context ) {
+            events.push( this );
+          }
+        } );
+
+        this.events[event] = events;
+
       }
-    }
 
-  };
+    };
+
+  } ).cache();
+
+  return Evy;
 
 } )();
